b1f36b99dec4528ade5ce95bf8a4cffaa66e9067
Brian Caterfino <bcaterfino@gmail.com>
2018-03-21 19:21:33 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,12 +3,12 @@
 def #display_rainbow(colors)
 puts "
 R: #{colors[0]},
-O: #{colors[0]},
-Y: yellow,
-G: green,
-B: blue,
-I: indigo,
-V: violet
+O: #{colors[1]},
+Y: #{colors[2]},
+G: #{colors[3]},
+B: #{colors[4]},
+I: #{colors[5]},
+V: #{colors[6]}
 
 "
 
